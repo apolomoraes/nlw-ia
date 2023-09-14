a0b08344360929e8b3a53f7fa9a66955ba19b171
Apolo Moraes <vlogsapolo@gmail.com>
2023-09-14 16:16:20 -0300

    complete generateAICompletion route and added fastify/cors

--- upload-ai-api/src/routes/generate-ai-completion.ts
+++ upload-ai-api/src/routes/generate-ai-completion.ts
@@ -6,11 +6,37 @@ import { openai } from "../lib/openai";
 import { json } from "stream/consumers";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
-  app.post('/videos/:videoId/transcription', async (request) => {
+  app.post('/ai/complete', async (request, reply) => {
     const bodySchema = z.object({
-      prompt: z.string(),
+      videoId: z.string().uuid(),
+      template: z.string(),
+      temperature: z.number().min(0).max(1).default(0.5),
     })
 
-    const { prompt } = bodySchema.parse(request.body);
+    const { videoId, template, temperature } = bodySchema.parse(request.body);
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      }
+    })
+
+    if (!video.transcription) {
+      return reply.status(400).send({
+        error: 'Video transcription was not generated yet.'
+      })
+    }
+
+    const promptMessage = template.replace('{transcription}', video.transcription);
+
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-16k',
+      temperature,
+      messages: [
+        { role: 'user', content: promptMessage }
+      ],
+    })
+
+    return response;
   })
 }
